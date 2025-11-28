87f77e5d00676004006538709e6b322ddc284aac
Bruno Anjos <r.brunoo457@gmail.com>
2025-11-28 13:35:06 -0300

    Adding IoT part

--- master/master.ino
+++ master/master.ino
@@ -1,5 +1,7 @@
 #include <pthread.h>
 #include "DHT.h"
+#include <WiFi.h>
+#include <PubSubClient.h>
 
 /* ######### Defines ######### */
 #define CIRCUIT_REFERENCE 3.3
@@ -22,6 +24,7 @@
 #define HUMIDITY_REFERENCE 45.00
 #define TEMPERATURE_REFERENCE 26.00
 
+
 /* ######### Types Definition ######### */
 typedef struct {
     float temperature;
@@ -29,19 +32,45 @@ typedef struct {
     float luminosity;
 } Sensors_t;
 
-/* ######### Global Variables ######### */
+typedef struct {
+    bool airing;
+    bool watering;
+    bool lightning;
+} Systems_t;
+
+
+/* ######### Global Variables/Constants ######### */
+/* ---------- CONFIG WIFI Constants ---------- */
+const char* ssid = "POCO X3 Pro";
+const char* password = "12345678";
+
+/* ---------- CONFIG MQTT (THINGSBOARD) ---------- */
+const char* mqttServer = "mqtt.thingsboard.cloud";
+const int mqttPort = 1883;
+const char* tbToken = "SistemasEmbarcados";
+const char* tbPassword = "grauB123";
+const char* tbTopic = "v1/devices/me/telemetry";
+
+/* ---------- Variables ---------- */
 Sensors_t greenhouseSensors = {0};
+Systems_t greenhouseSystems = {0};
 DHT dhtSensor(DHT_PIN, DHT_TYPE);
+WiFiClient espWifiClient;
+PubSubClient mqttClient(espWifiClient);
 
 /* ######### Private functions ######### */
+pthread_mutex_t sensorMutex = PTHREAD_MUTEX_INITIALIZER;
 
+/* ######### Private functions ######### */
 static void checkAiringSystem(){
   // Check Temperature
   if(greenhouseSensors.temperature > TEMPERATURE_REFERENCE){
     digitalWrite(OUT_AIRING_PIN, HIGH);
+    greenhouseSystems.airing = true;
   }
   else{
     digitalWrite(OUT_AIRING_PIN, LOW);
+    greenhouseSystems.airing = false;
   }
 }
 
@@ -49,9 +78,11 @@ static void checkWateringSystem(){
   // Check Luminosity
   if(greenhouseSensors.humidity < HUMIDITY_REFERENCE){
     digitalWrite(OUT_WATERING_PIN, HIGH);
+    greenhouseSystems.watering = true;
   }
   else{
     digitalWrite(OUT_WATERING_PIN, LOW);
+    greenhouseSystems.watering = false;
   }
 }
 
@@ -59,9 +90,11 @@ static void checkLightningSystem(){
   // Check Luminosity
   if(greenhouseSensors.luminosity < LUMINOSITY_REFERENCE){
     digitalWrite(OUT_LIGHTNING_PIN, HIGH);
+    greenhouseSystems.lightning = true;
   }
   else{
     digitalWrite(OUT_LIGHTNING_PIN, LOW);
+    greenhouseSystems.lightning = false;
   }
 }
 
@@ -79,34 +112,41 @@ static float readLuminosity(){
   float voltage = (adcValue * CIRCUIT_REFERENCE)/ADC_RESOLUTION;
   if (voltage <= 0.0001) voltage = 0.0001;
 
-  //float resistance = LDR_RFIXED * (CIRCUIT_REFERENCE/(voltage - 1));
-  //float lux = pow((LDR_A/LDR_RFIXED), (1.0/LDR_B));
-
   return voltage;
 }
+
+static String buildPayload(){
+  String payload = String("{\"temperatura\":") + String(greenhouseSensors.temperature, 2)
+                + String(",\"umidade\":") + String(greenhouseSensors.humidity, 2)
+                + String(",\"luminosidade\":") + String(greenhouseSensors.luminosity, 2)
+                + String(",\"ventilacao\":") + (greenhouseSystems.airing ? "true" : "false")
+                + String(",\"irrigacao\":") + (greenhouseSystems.watering ? "true" : "false")
+                + String(",\"iluminacao\":") + (greenhouseSystems.lightning ? "true" : "false")
+                + String("}");
+
+  return payload;
+} 
 
 /* ######### Threads Definition ######### */
 void* t_readings(void *arg) {
   while(1){
-      // Read Temperature
+      pthread_mutex_lock(&sensorMutex);
+      
       greenhouseSensors.temperature = dhtSensor.readTemperature();
-      // Read Humidity
       greenhouseSensors.humidity = dhtSensor.readHumidity();
-      // Read Luminosity
       greenhouseSensors.luminosity = readLuminosity();
 
-      // Checking DHT reading failure
       if (isnan(greenhouseSensors.temperature) || isnan(greenhouseSensors.humidity)) {
         Serial.println("Falha na leitura do sensor DHT11!");
         break;
       }
 
-      // Check values to set output systems
       checkAiringSystem();
       checkWateringSystem();
       checkLightningSystem();
 
-      // Debug
+      pthread_mutex_unlock(&sensorMutex);
+
       Serial.println("#########################");
       Serial.print("Reading Temperature = ");
       Serial.print(greenhouseSensors.temperature);
@@ -119,14 +159,72 @@ void* t_readings(void *arg) {
       Serial.println(" lux");
       Serial.println("#########################");
 
-      delay(READING_TIME_S);
+      usleep((useconds_t)READING_TIME_S * 1000);
   }
+
+  return NULL;
 }
+
+void* t_iot(void *arg){
+  mqttClient.setServer(mqttServer, mqttPort);
+
+  while(1) {
+    /* ----------------- WIFI ----------------- */
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.println("[WiFi] Conectando...");
+      WiFi.mode(WIFI_STA);
+      WiFi.begin(ssid, password);
+
+      int tentativas = 0;
+      while (WiFi.status() != WL_CONNECTED && tentativas < 20) {
+        Serial.print(".");
+        usleep(300 * 1000);
+        tentativas++;
+      }
+
+      if (WiFi.status() == WL_CONNECTED) {
+        Serial.println("\n[WiFi] Conectado: " + WiFi.localIP().toString());
+      } else {
+        Serial.println("\n[WiFi] Falhou!");
+        usleep(2000 * 1000);
+        continue;
+      }
+    }
+
+    /* ----------------- MQTT ----------------- */
+    if (!mqttClient.connected()) {
+      Serial.println("[MQTT] Conectando ao servidor...");
+      if (mqttClient.connect("teste", tbToken, tbPassword)) {
+        Serial.println("[MQTT] Conectado!");
+      } else {
+        Serial.println("[MQTT] Falhou ao conectar. Tentando novamente...");
+        usleep(2000 * 1000);
+        continue;
+      }
+    }
+
+    pthread_mutex_lock(&sensorMutex);
+    String payload = buildPayload();
+    pthread_mutex_unlock(&sensorMutex);
+
+    if (mqttClient.publish(tbTopic, payload.c_str())) {
+      Serial.println("[MQTT] Enviado -> " + payload);
+    } else {
+      Serial.println("[MQTT] Falha ao publicar!");
+    }
+
+    mqttClient.loop();
+    usleep(6000 * 1000);
+  }
+
+  return NULL;
+}
+
 
 /* #########  Setup ######### */
 void setup() {
   // Set main theards
-  pthread_t thread1;
+  pthread_t thread1, thread2;
 
   // Debug Serial:
   Serial.begin(115200);
@@ -146,10 +244,17 @@ void setup() {
   dhtSensor.begin();
 
   // Create threads
-  pthread_create(&thread1, NULL, &t_readings, NULL);
+  if(!pthread_create(&thread1, NULL, &t_readings, NULL)){
+    Serial.print("### Erro ao criar thread readings");
+  }else{
+    pthread_detach(thread1);
+  }
 
-  // Start threads
-  pthread_join(thread1, NULL);
+  if (pthread_create(&thread2, NULL, &t_iot, NULL)) {
+    Serial.print("### Erro ao criar thread iot");
+  } else {
+    pthread_detach(thread2);
+  }
 
 }
 
