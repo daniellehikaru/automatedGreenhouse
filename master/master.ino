77ba7412dc3dcf7e8591cd715937b32045ccd22e
Bruno Anjos <r.brunoo457@gmail.com>
2025-11-27 22:43:11 -0300

    Change file format and adding some adjusts

--- master/master.ino
+++ master/master.ino
@@ -14,6 +14,14 @@
 #define LDR_B 0.7        // constante de calibração
 #define LDR_RFIXED 10000.0
 
+#define OUT_LIGHTNING_PIN 12
+#define OUT_WATERING_PIN 13
+#define OUT_AIRING_PIN 14
+
+#define LUMINOSITY_REFERENCE 2.00
+#define HUMIDITY_REFERENCE 45.00
+#define TEMPERATURE_REFERENCE 26.00
+
 /* ######### Types Definition ######### */
 typedef struct {
     float temperature;
@@ -26,6 +34,36 @@ Sensors_t greenhouseSensors = {0};
 DHT dhtSensor(DHT_PIN, DHT_TYPE);
 
 /* ######### Private functions ######### */
+
+static void checkAiringSystem(){
+  // Check Temperature
+  if(greenhouseSensors.temperature > TEMPERATURE_REFERENCE){
+    digitalWrite(OUT_AIRING_PIN, HIGH);
+  }
+  else{
+    digitalWrite(OUT_AIRING_PIN, LOW);
+  }
+}
+
+static void checkWateringSystem(){
+  // Check Luminosity
+  if(greenhouseSensors.humidity < HUMIDITY_REFERENCE){
+    digitalWrite(OUT_WATERING_PIN, HIGH);
+  }
+  else{
+    digitalWrite(OUT_WATERING_PIN, LOW);
+  }
+}
+
+static void checkLightningSystem(){
+  // Check Luminosity
+  if(greenhouseSensors.luminosity < LUMINOSITY_REFERENCE){
+    digitalWrite(OUT_LIGHTNING_PIN, HIGH);
+  }
+  else{
+    digitalWrite(OUT_LIGHTNING_PIN, LOW);
+  }
+}
 
 // Will be necessary to adjust this method depending the LDR that we use (is implemented now to the LDR of Wokwi simulator)
 static float readLuminosity(){
@@ -40,17 +78,11 @@ static float readLuminosity(){
   float adcValue = (float) sumAdcValues/samples;
   float voltage = (adcValue * CIRCUIT_REFERENCE)/ADC_RESOLUTION;
   if (voltage <= 0.0001) voltage = 0.0001;
-  float resistance = LDR_RFIXED * (CIRCUIT_REFERENCE/(voltage - 1));
-  float lux = pow((LDR_A/LDR_RFIXED), (1.0/LDR_B));
 
-  Serial.print("LDR = ADC = ");
-  Serial.println(adcValue);
-  Serial.print("LDR = Voltage = ");
-  Serial.println(voltage);
-  Serial.print("LDR = Resistence = ");
-  Serial.println(resistance);
+  //float resistance = LDR_RFIXED * (CIRCUIT_REFERENCE/(voltage - 1));
+  //float lux = pow((LDR_A/LDR_RFIXED), (1.0/LDR_B));
 
-  return lux;
+  return voltage;
 }
 
 /* ######### Threads Definition ######### */
@@ -69,6 +101,13 @@ void* t_readings(void *arg) {
         break;
       }
 
+      // Check values to set output systems
+      checkAiringSystem();
+      checkWateringSystem();
+      checkLightningSystem();
+
+      // Debug
+      Serial.println("#########################");
       Serial.print("Reading Temperature = ");
       Serial.print(greenhouseSensors.temperature);
       Serial.println(" °C");
@@ -78,6 +117,7 @@ void* t_readings(void *arg) {
       Serial.print("Reading Luminosity = ");
       Serial.print(greenhouseSensors.luminosity);
       Serial.println(" lux");
+      Serial.println("#########################");
 
       delay(READING_TIME_S);
   }
@@ -91,6 +131,16 @@ void setup() {
   // Debug Serial:
   Serial.begin(115200);
   Serial.println("Initializing...");
+
+  // Set outputs pins
+  pinMode(OUT_LIGHTNING_PIN, OUTPUT);
+  pinMode(OUT_WATERING_PIN, OUTPUT);
+  pinMode(OUT_AIRING_PIN, OUTPUT);
+
+  // Set initial output states
+  digitalWrite(OUT_AIRING_PIN, LOW);
+  digitalWrite(OUT_WATERING_PIN, LOW);
+  digitalWrite(OUT_LIGHTNING_PIN, LOW);
 
   // Start DHT
   dhtSensor.begin();
